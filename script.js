4631b4e84baaae5635b043be6bc651d2c6a60d5b
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2017-12-28 01:41:56 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    var comment = document.getElementById("comment")
+    var user = document.getElementById("user")
+    var push = document.getElementById("push")
+
+    push.onclick = function () {
+        console.log('!')
+    }
+}
