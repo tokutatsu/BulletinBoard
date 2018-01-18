137121015baeb84221e9ec5fd11eabc437496d64
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2018-01-19 03:54:50 +0900

    投稿を表示

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ window.onload = function () {
     var comment = document.getElementById("comment")
     var user = document.getElementById("user")
     var push = document.getElementById("push")
-    var now = new Date()
+    var output = document.getElementsByName("output")
 
     push.onclick = function () {
-        console.log(user.value)
-        console.log(comment.value)
+
         var now = new Date()
         var year = now.getFullYear()
-        var month = now.getMonth()
+        var month = now.getMonth() + 1
         var date = now.getDate()
         var hour = now.getHours()
         var min = now.getMinutes()
         if (min < 10) {
             min = '0' + min
         }
-        console.log(year + '/' +  month + '/' + date + ' ' + hour + ':' + min)
+        var time = year + '/' +  month + '/' + date + ' ' + hour + ':' + min
+        for (i = 9; i > 0; i--) {
+            output[i].textContent = output[i - 1].textContent
+        }
+        output[0].textContent = time + '  ' + user.value + '  ' + comment.value
+
     }
 }
